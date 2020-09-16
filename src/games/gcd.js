2620928bba1a5d2b13ad5c1152b5f6bb0cdbd306
Sergey Kirillov <s_kirillov@rambler.ru>
2020-09-16 14:12:57 +0300

    fixed linter errors

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -8,9 +8,9 @@ const gcd = (number1, number2) => {
 
   while (num1 !== 0 && num2 !== 0) {
     if (num1 > num2) {
-      num1 = num1 % num2;
+      num1 %= num2;
     } else {
-      num2 = num2 % num1;
+      num2 %= num1;
     }
   }
 
